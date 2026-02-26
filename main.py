7d722e9c813842c78e795d51b8c1efb420b638a9
Kenneth <kensinght@gmail.com>
2026-02-27 02:51:54 +0530

    Modified max tokens

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def format_prompt(query, context):
 
 import torch
 
-def call_answer_llm(prompt, max_new_tokens=500):
+def call_answer_llm(prompt, max_new_tokens=250):
     inputs = tokenizer(
         prompt,
         return_tensors="pt"

9da5342e8ef9c5c5bd6463f74d9a817c16098d34
Kenneth <kensinght@gmail.com>
2026-02-27 02:25:27 +0530

    Modified input prompt

--- main.py
+++ main.py
@@ -136,7 +136,6 @@ def format_prompt(query, context):
   return f"""
   You are a financial analyst assistant.
   Be precise and answer using only the provided context.
-  Do NOT make assumptions.
   If answer is not found, ONLY say "This question cannot be answered based on the provided documents."
   
   Context:

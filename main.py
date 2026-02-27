2bc6f429dd79e5506e0efc9778dcd0a06300b4e6
Kenneth <kensinght@gmail.com>
2026-02-28 03:48:25 +0530

    changed llm

--- main.py
+++ main.py
@@ -124,8 +124,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 # model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 # model_id = "Qwen/Qwen2.5-0.5B-Instruct"
 # model_id = "openai/gpt-oss-120b"
-# model_id = "Qwen/Qwen2.5-7B-Instruct"
-model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+model_id = "Qwen/Qwen2.5-7B-Instruct"
+# model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
 # model_id = "deepseek-ai/DeepSeek-R1-Zero"
 # model_id = "Qwen/Qwen3-14B"
 

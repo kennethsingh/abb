b8a47b8e2c34c154703ce0f435cf4670a18c042f
Kenneth <kensinght@gmail.com>
2026-02-27 12:51:50 +0530

    page number fix

--- main.py
+++ main.py
@@ -188,7 +188,9 @@ def answer_question(query: str) -> dict:
   # print(docs[0].metadata)
   sources =[]
   for doc in docs:
-    sources.append((doc.metadata['document'], "Item ??", f"page. {doc.metadata['page_label']}"))
+    print(f"page_number. {doc.metadata['page_label']}")
+    sources.append((doc.metadata['document'], "Item ??", f"page_number. {doc.metadata['page_label']}"))
+    
 
   context = "\n\n".join([
       f"[Source: {doc.metadata['document']}, Page: {doc.metadata['page_label']}]\n{doc.page_content}"

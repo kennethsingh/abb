e45ecb92acda6d2c97a655b692c11bdf8c079cf3
Kenneth <kensinght@gmail.com>
2026-02-27 12:38:53 +0530

    Corrected page label in source

--- main.py
+++ main.py
@@ -188,10 +188,10 @@ def answer_question(query: str) -> dict:
   # print(docs[0].metadata)
   sources =[]
   for doc in docs:
-    sources.append((doc.metadata['document'], "Item ??", f"p. {doc.metadata['page']}"))
+    sources.append((doc.metadata['document'], "Item ??", f"p. {doc.metadata['page_label']}"))
 
   context = "\n\n".join([
-      f"[Source: {doc.metadata['document']}, Page: {doc.metadata['page']}]\n{doc.page_content}"
+      f"[Source: {doc.metadata['document']}, Page: {doc.metadata['page_label']}]\n{doc.page_content}"
       for doc in docs
   ])
 
